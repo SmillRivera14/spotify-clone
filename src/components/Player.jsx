e9c9e4c475218fd4fb43617e3c736cc0a218e195
SmillRivera14 <smillriveradiaz14@gmail.com>
2025-02-24 16:55:37 -0400

    reestructurando

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,21 +2,7 @@ import { Play, Pause } from "@/icons/PlayerIcons";
 import { usePlayerStore } from "@/store/playerStore";
 import { useEffect, useRef } from "react";
 import { PlayerVolumeControl } from "./PlayerVolumeControl";
-
-const CurrentSong = ({ image, title, artists }) => {
-  return (
-    <div className="flex items-center gap-5 relative">
-      <picture className="w-16 h-16 bg-zinc-800 rounded-md shadow-lg overflow-hidden">
-        <img src={image} alt={title} />
-      </picture>
-
-      <div className="flex flex-col">
-        <h3 className="font-semibold text-sm block">{title}</h3>
-        <span className="text-xs opacity-80">{artists?.join(", ")}</span>
-      </div>
-    </div>
-  );
-};
+import { PlayerCurrentSong } from "./PlayerCurrentSong";
 
 export function Player() {
   const { isPlaying, setIsPlaying, currentMusic, volume } = usePlayerStore(
@@ -49,7 +35,7 @@ export function Player() {
   return (
     <div className="flex flex-row justify-between w-full px-1 z-50">
       <div className="w-[200px]">
-        <CurrentSong {...currentMusic.song} />
+        <PlayerCurrentSong {...currentMusic.song} />
       </div>
       <div className="grid place-content-center gap-4 flex-1">
         <div className="flex justify-center">
